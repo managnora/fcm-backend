1972d5ffcc58d0c0da3d3f67e1ce89c49a02e9bb
jean-soa.magnanora <jean-soa.magnanora@compagnie-fiduciaire.com>
2025-01-15 10:00:02 +0300

    fix add new fcmToken

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const admin = require("./src/firebaseService");
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 
 const validateNotificationData = (data) => {
     return data && data.isVideo && data.type && data.roomId;
 };
+
 const sanitizeData = (data) => {
     const sanitizedData = {};
     for (const key in data) {
@@ -34,26 +34,22 @@ const sendNotification = async (tokens, payload, data) => {
         throw error;
     }
 };
-// Route pour envoyer des notifications
 app.post("/send-notification", async (req, res) => {
     const { senderId, receiverIds, data } = req.body;
-    console.log('senderId ', senderId);
-    console.log('receiverIds ', receiverIds);
-    console.log('data ', data);
-    // Validation des données
+
     if (!senderId || !receiverIds || !validateNotificationData(data)) {
         return res.status(400).send("Tous les champs (senderId, receiverIds, et données valides) sont nécessaires.");
     }
 
     const payload = {
-        title: ' Rasoa ',
+        title: 'Rasoa',
         body: `Appel ${data.isVideo ? 'video' : 'audio'} entrant`,
     };
 
-    // Simule les tokens des récepteurs (ici il faut les récupérer depuis la base de données)
     const receiverTokens = [
         'dollRlGgRBqhpV6kquSGXW:APA91bFGkg2RBixvT_I5I1zP18aSYd1VmqKhRLH1RHHR8KBXdulPr9vlE-_flGLwyCwF9-VT9OEnZsRV7-OVI8B5XdeNf_O-tFNqsCqKrN_kRvshScFkFVg',
-        'f0h_yZkOQSeQwmJr7KRQy-:APA91bECDe5D9LaOgoh0ep067rXYtUE5zYxAjx3AHyCcgwE2oC1agzx8-JEw1cWfx06N-d9FaAkI56lrHzO-BvkSj0UJv_KBaGiRkba3VQqPmryQTC_zydQ'
+        'f0h_yZkOQSeQwmJr7KRQy-:APA91bECDe5D9LaOgoh0ep067rXYtUE5zYxAjx3AHyCcgwE2oC1agzx8-JEw1cWfx06N-d9FaAkI56lrHzO-BvkSj0UJv_KBaGiRkba3VQqPmryQTC_zydQ',
+        'fyNliZs-SMeqILiX3khyIN:APA91bHHupop6MJol9IXNbxuoIwcawTq0cnCBP3mkay0wALS3VNHPqk3VqCWWGERm4jBOcxB-YgQd_0XnE-djMVSJkOvD82ns7IvTkWVFjKTXUQUrAxQmkM'
     ];
 
     try {
